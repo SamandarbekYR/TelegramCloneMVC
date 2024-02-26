8cbba1f09acd63a46869c009474ef42eb39d37ba
SamandarbekYr <SamandarbekYR@gmail.com>
2024-02-26 23:29:09 +0500

    MVC: Send SMS Ui created

--- TelegramCloneMVC/Program.cs
+++ TelegramCloneMVC/Program.cs
@@ -35,6 +35,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Register}/{id?}");
 
 app.Run();
